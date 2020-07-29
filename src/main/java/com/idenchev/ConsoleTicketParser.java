11666a65e849a445c62b795c50bce55198787d96
iVan <i@i>
2020-07-29 18:13:09 +0300

    Adjusted FileMalwareParser to use InputStream instead of File

--- src/main/java/com/idenchev/ConsoleTicketParser.java
+++ src/main/java/com/idenchev/ConsoleTicketParser.java
@@ -2,7 +2,17 @@ package com.idenchev;
 
 import com.idenchev.io.config.AppConfig;
 import com.idenchev.io.config.YmlAppConfig;
+import com.idenchev.malware.FileMalwareParser;
+import com.idenchev.malware.LineParser;
+import com.idenchev.malware.LineParserImpl;
 import com.idenchev.malware.MalwareParser;
+import com.idenchev.malware.validate.BeginsWithValidator;
+import com.idenchev.malware.validate.CPanelUsernameValidator;
+import com.idenchev.malware.validate.ContainsValidator;
+import com.idenchev.malware.validate.EndsWithValidator;
+import com.idenchev.malware.validate.LineValidatorChain;
+
+import java.io.InputStream;
 
 public class ConsoleTicketParser extends TicketParser {
 
@@ -17,6 +27,16 @@ public class ConsoleTicketParser extends TicketParser {
 
     @Override
     MalwareParser makeMalwareParser() {
-        return null;
+        LineValidatorChain lineValidatorChain = new LineValidatorChain();
+        lineValidatorChain.addValidator(new CPanelUsernameValidator(appConfig.getIgnoreCPanelUserList()));
+        lineValidatorChain.addValidator(new BeginsWithValidator(appConfig.getIgnoreIfBeginsWithAnyList()));
+        lineValidatorChain.addValidator(new ContainsValidator(appConfig.getIgnoreIfContainsAnyList()));
+        lineValidatorChain.addValidator(new EndsWithValidator(appConfig.getIgnoreIfEndsWithAnyList()));
+        LineParser lineParser = new LineParserImpl(lineValidatorChain);
+        InputStream scanFileStream = getClass().getClassLoader().getResourceAsStream("scan.txt");
+        return new FileMalwareParser(
+                scanFileStream,
+                lineParser
+        );
     }
 }
