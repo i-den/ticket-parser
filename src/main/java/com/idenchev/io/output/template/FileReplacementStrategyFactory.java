46094e4b00425d9f5df510852dde5e54a2016c88
iVan <i@i>
2020-07-30 17:38:09 +0300

    Adjusted FileReplacementStrategyFactory to use cached strategies Added README.md removed SNAPSHOT from pom.xml

--- src/main/java/com/idenchev/io/output/template/FileReplacementStrategyFactory.java
+++ src/main/java/com/idenchev/io/output/template/FileReplacementStrategyFactory.java
@@ -2,20 +2,23 @@ package com.idenchev.io.output.template;
 
 import com.idenchev.io.config.AppConfig;
 import com.idenchev.malware.InfectedUser;
-import com.idenchev.io.config.AppConfig;
 
 public class FileReplacementStrategyFactory {
     AppConfig appConfig;
+    PastebinSiteFileReplacementStrategy pastebinSiteFileReplacementStrategy;
+    JoiningFileReplacementStrategy joiningFileReplacementStrategy;
 
     public FileReplacementStrategyFactory(AppConfig appConfig) {
         this.appConfig = appConfig;
+        pastebinSiteFileReplacementStrategy = new PastebinSiteFileReplacementStrategy(appConfig.getApiKey());
+        joiningFileReplacementStrategy = new JoiningFileReplacementStrategy();
     }
 
     FileReplacementStrategy getStrategy(InfectedUser user) {
         if (user.getFiles().size() > appConfig.getMaxScanFiles()) {
-            return new PastebinSiteFileReplacementStrategy(appConfig.getApiKey());
+            return pastebinSiteFileReplacementStrategy;
         } else {
-            return new JoiningFileReplacementStrategy();
+            return joiningFileReplacementStrategy;
         }
     }
 }
